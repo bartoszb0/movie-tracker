12ec4aac1654fe34c69cc9ebbb815a9c86389480
bartoszb0 <bartoszb2020@gmail.com>
2026-06-10 23:41:05 +0200

    feat: implement registration and login functionality with JWT and error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,72 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '../../generated/prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  register(body: RegisterDto) {
+  async register(body: RegisterDto) {
     if (body.password !== body.confirmPassword) {
       throw new BadRequestException('Passwords do not match');
     }
-    return 'this will register user';
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: body.email,
+          password: hashedPassword,
+        },
+      });
+      return {
+        acces_token: await this.generateToken(user.id, user.email),
+        id: user.id,
+        email: user.email,
+      };
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Email already in use');
+      }
+      throw error;
+    }
   }
 
-  login(body: LoginDto) {
-    return `this will login user`;
+  async login(body: LoginDto) {
+    const match = await this.prisma.user.findFirst({
+      where: { email: body.email },
+    });
+
+    if (!match) throw new UnauthorizedException('Invalid credentials');
+
+    const compareResult = await bcrypt.compare(body.password, match.password);
+
+    if (!compareResult) throw new UnauthorizedException('Invalid credentials');
+
+    return {
+      acces_token: await this.generateToken(match.id, match.email),
+      id: match.id,
+      email: match.email,
+    };
+  }
+
+  private async generateToken(userId: string, email: string) {
+    return this.jwtService.signAsync({ sub: userId, email });
   }
 }

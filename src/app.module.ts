0252469031863db80e203507a63a3b8160a75372
bartoszb0 <bartoszb2020@gmail.com>
2026-06-11 17:54:37 +0200

    add watchlist resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
+import { WatchlistModule } from './watchlist/watchlist.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, MoviesModule],
+  imports: [PrismaModule, AuthModule, MoviesModule, WatchlistModule],
   controllers: [],
   providers: [],
 })

19527f539e8c3f90a6288670b509966abb8ca0de
bartoszb0 <bartoszb2020@gmail.com>
2026-06-11 17:59:38 +0200

    remove dto files for watchlist

--- src/watchlist/dto/update-watchlist.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWatchlistDto } from './create-watchlist.dto';
-
-export class UpdateWatchlistDto extends PartialType(CreateWatchlistDto) {}
